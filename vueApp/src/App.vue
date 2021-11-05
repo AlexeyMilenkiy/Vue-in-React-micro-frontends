7e4d0cde0e1f7f808b7a7c287e89665af9864642
AlexeyMilenkiy <alex.milenkiy@gmail.com>
2021-11-05 22:17:17 +0300

    init commit

---
+++ vueApp/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>React in Vue</h1>
+    <div class="app">
+      <layout />
+    </div>
+</template>
+
+<script>
+import Layout from "./Layout";
+
+export default {
+  components: {
+    layout: Layout,
+  },
+};
+
+</script>
+
+<style>
+
+</style>
